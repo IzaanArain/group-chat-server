35934be23cdb5dc691dd414860ac4f4fda009033
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-01-03 16:47:32 +0500

    updated

--- routes/User.js
+++ routes/User.js
@@ -8,7 +8,9 @@ const {
   forgetPassword,
   resetPassword,
   completeProfile,
-  changePassword
+  changePassword,
+  signOut,
+  deleteProfile
 } = require("../controllers/User");
 const userAuth=require("../middlewares/Auth");
 const {upload}=require("../middlewares/Multer");
@@ -23,5 +25,7 @@ router.post("/user/forgetPassword", forgetPassword);
 router.post("/user/resetPassword", resetPassword);
 router.post("/user/completeProfile", userAuth, upload.fields([{name:"profileImage"}]), completeProfile);
 router.post("/user/changePassword", userAuth, changePassword);
+router.post("/user/deleteProfile", userAuth, deleteProfile);
+router.post("/user/signOut", userAuth, signOut);
 
 module.exports = router;
