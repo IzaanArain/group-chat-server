bd4fc2da646816a0f0c25f2ae66454a195dc87d1
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-01-03 13:40:51 +0500

    updated

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,38 +1,34 @@
 const jwt = require("jsonwebtoken");
-const mongoose=require("mongoose");
-const User=mongoose.model("User");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 module.exports = (req, res, next) => {
-    const authorization = req?.headers?.authorization || req?.headers?.["Authorization"];
-    if (!authorization) {
-      return res.status(401).send({ message: "unauthorized" })
+  const authorization = req?.headers?.authorization || req?.headers?.["Authorization"];
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  jwt.verify(token, process.env.SECRET_TOKEN, async (err, payload) => {
+    if (err) {
+      return res.status(401).send({ status: 0, message: "unauthorized" });
     }
-    const token = authorization.replace("Bearer ", "");
-    jwt.verify(token, process.env.SECRET_TOKEN, async (err, payload) => {
-        if (err) {
-          return res.status(401).send({ status: 0, message: "unauthorized" });
-        }
-        const { userId } = payload;
-        const user = await User.findById(userId)
-        if (!user) {
-          return res.status(401).send({ status: 0, message: "unauthorized" });
-        }
-        else if (user.token !== token) {
-          return res.status(401).send({ status: 0, message: "unauthorized" });
-        }
-        else if (user.isVerified == 0) {
-          return res.status(400).send({ status: 0, message: "User is not verified" });
-        }
-        else if (user.isBlocked == 1) {
-          return res.status(401).send({ status: 0, message: "User is blocked"});
-        }
-        else if (user.isDeleted == 1) {
-          return res.status(401).send({ status: 0, message: "User is deleted"});
-        }
-        else if (user.token == token) {
-          req.user = user;
-          next();
-        }
-    
-      });
+    const { userId } = payload;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).send({ status: 0, message: "unauthorized" });
+    } else if (user.token !== token) {
+      return res.status(401).send({ status: 0, message: "unauthorized" });
+    } else if (user.isVerified == 0) {
+      return res
+        .status(400)
+        .send({ status: 0, message: "User is not verified" });
+    } else if (user.isBlocked == 1) {
+      return res.status(401).send({ status: 0, message: "User is blocked" });
+    } else if (user.isDeleted == 1) {
+      return res.status(401).send({ status: 0, message: "User is deleted" });
+    } else if (user.token == token) {
+      req.user = user;
+      next();
+    }
+  });
 };
