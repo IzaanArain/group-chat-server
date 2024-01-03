ae62f39f68d1fafb4e9a7423b87447bc241c3248
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-01-03 16:08:45 +0500

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRoutes);
-// app.use(express.static("uploads"));
+app.use("/uploads",express.static("uploads"));
 
 PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
