a9d04c7e94cdb6ba4cb36950007d67fd6d55db21
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-01-11 14:52:53 +0500

    updated

--- models/GroupChat.js
+++ models/GroupChat.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose");
 
-const groupChatSchema = mongoose.Schema({
-  chatName: {
-    type: String,
-    trim: true,
-    default: null,
-  },
-  isGroupChat: {
-    type: Number,
-    enum:[0,1],
-    default: 0,
-  },
-  groupMember: [
-    {
+const groupChatSchema = mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    isGroupChat: {
+      type: Number,
+      enum: [0, 1],
+      default: 0,
+    },
+    groupMember: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: null,
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "chat",
+      default: null,
+    },
+    groupAdmin: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       default: null,
     },
-  ],
-  latestMessage: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "chat",
-    default: null,
   },
-  groupAdmin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    default: null,
-  },
-},{ timestamps: true });
+  { timestamps: true }
+);
 
-mongoose.model("groupChat",groupChatSchema);
+mongoose.model("groupChat", groupChatSchema);
 // module.exports= mongoose.model("groupChat",groupChatSchema);
